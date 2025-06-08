4aeb87b724a4b11950a0ca47b20c5605a89617b8
SSR14FEB <instaheri6@gmail.com>
2025-06-08 09:13:06 +0530

    fix(task): Improve error handling in createTask and adminDashboard functions

--- Backend/src/controllers/taskControllers.js
+++ Backend/src/controllers/taskControllers.js
@@ -9,7 +9,7 @@ const adminDashboard = asyncHandler(async (req, res) => {
         "name role email profileImageUrl"
     );
     if (!adminProfile) {
-        return apiError(404, "Admin not found");
+        throw apiError(404, "Admin not found");
     }
     const totalTask = await Task.countDocuments();
     const pendingTask = await Task.countDocuments({ status: "Pending" });
@@ -93,6 +93,24 @@ const createTask = asyncHandler(async (req, res) => {
         status,
         Progress,
     } = req.body;
+
+    if (
+        [
+            title,
+            description,
+            createdBy,
+            dueDate,
+            Priority,
+            status,
+            Progress,
+        ].some((field) => field?.trim() == "")
+    ) {
+        throw new apiError(400, "All fields are required");
+    }
+
+    if (!Array.isArray(assignTo)) {
+        throw new apiError(400, "assignTo must be an array");
+    }
     const createdTask = await Task.create({
         title: title,
         description: description,
@@ -104,19 +122,10 @@ const createTask = asyncHandler(async (req, res) => {
         status: status,
         Progress: Progress,
     });
-    return res.status(202, "Task created successfully", {
-        task: {
-            title: title,
-            description: description,
-            createdBy: createdBy,
-            assignTo: assignTo,
-            todoCheckList: todoCheckList,
-            dueDate: dueDate,
-            Priority: Priority,
-            status: status,
-            Progress: Progress,
-        },
-    });
+
+    return res
+        .status(202)
+        .json(new apiResponse(202, "Task created successfully", createdTask));
 });
 
 const getTask = asyncHandler(async (req, res) => {
@@ -130,42 +139,43 @@ const getTask = asyncHandler(async (req, res) => {
     if (req.user.role == "admin") {
         tasks = await Task.find(filter).populate(
             "assignTo",
-            "name, email, profileImageUrl"
+            "name email profileImageUrl"
         );
     }
     if (req.user.role == "member") {
-        tasks = await Task.find({ ...filter, assignTo: req.user._id }).populate(
+        tasks = await Task.find({...filter, assignTo: req.user._id }).populate(
             "assignTo",
-            "name, email, profileImageUrl"
+            "name email profileImageUrl"
         );
     }
+
 
     tasks = await Promise.all(
         tasks.map(async (tasks) => {
             const completedCount = tasks.todoCheckList.filter(
-                (item) => item.completed == true
+                (item) => item.completed 
             ).length;
             return { ...tasks._doc, completedTodoCount: completedCount };
         })
     );
     // task status summary
     const allTask = await Task.countDocuments(
-        req.user._id == "admin" ? {} : { assignTo: req.user._id }
+        req.user.role === "admin" ? {} : { assignTo: req.user._id }
     );
     const pendingTask = await Task.countDocuments({
         ...filter,
         status: "Pending",
-        ...(req.user.role != "admin" && { assignTo: req.user._id }),
+        ...(req.user.role !== "admin" && { assignTo: req.user._id }),
     });
     const inProgressTask = await Task.countDocuments({
         ...filter,
         status: "In Progress",
-        ...(req.user.role != "admin" && { assignTo: req.user._id }),
+        ...(req.user.role !== "admin" && { assignTo: req.user._id }),
     });
     const completedTask = await Task.countDocuments({
         ...filter,
         status: "Completed",
-        ...(req.user.role != "admin" && { assignTo: req.user._id }),
+        ...(req.user.role !== "admin" && { assignTo: req.user._id }),
     });
 
     return res.status(200).json(
