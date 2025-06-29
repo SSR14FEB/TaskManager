b086b21d3db4048e292c6bd9516444af3ee453e7
SSR14FEB <instaheri6@gmail.com>
2025-06-29 17:32:11 +0530

    feat(auth): Refactor Login and AuthLayout components for improved layout and validation

--- Frontend/Task-Manager/src/components/layouts/AuthLayout.jsx
+++ Frontend/Task-Manager/src/components/layouts/AuthLayout.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 function AuthLayout({ children }) {
   return (
-    <div className="flex items-center">
-      <div className="w-screen h-screen md:w-[60vw] px-12 pt-12 pb-12 ">
-        <h1 className="text-xl font-bold">TaskManger</h1>
+    <div className="flex">
+      <div className="w-screen h-screen flex flex-col justify-center gap-[100px] md:flex md:justify-around md:w-[60vw] px-12 pt-12 pb-12 ">
+        <h1 className="text-xl font-bold  text-indigo-500">TaskManger</h1>
         {children}
       </div>
-      <div className="hidden md:flex w-[40vw] h-screen items-center  ">
-        <img className="object-cove"
+      <div className="hidden md:flex w-[40vw] h-screen items-center">
+        <img className="object-cover"
           src="https://res.cloudinary.com/dabcrcxpu/image/upload/v1750912261/209089066_10920011_k61dtw.jpg"
           alt="error 404 image not found"
         />
