cfcea70c347af91ce5b2e51e1c3ca6821abfdb46
SSR14FEB <instaheri6@gmail.com>
2025-08-16 19:09:37 +0530

    feat(task): add Manage Task setup

--- Frontend/Task-Manager/src/pages/Admin/ManageTasks.jsx
+++ Frontend/Task-Manager/src/pages/Admin/ManageTasks.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
-
+import { useState, useEffect } from "react";
+import DashboardLayout from "../../components/layouts/DashboardLayout";
+import { useNavigate } from "react-router-dom";
 function ManageTasks() {
-  return (
-    <div>
-      <h2>Manage Task</h2>
-    </div>
-  )
+  const [allTask, setAllTask] = useState([]);
+  const [tabs, setTabs] = useState([]);
+  const [filterStatus, setFilterStatus] = useState("All");
+
+  const navigate = useNavigate();
+
+  const getAllTask = async () => {};
+
+  const handelClick = (taskData) => {
+    navigate(`/admin/create_tasks`, { state: { taskId: taskData } });
+  };
+
+  const handelDownloadReport = async () => {};
+
+  useEffect(() => {
+    getAllTask(filterStatus);
+    return () => {};
+  }, [filterStatus]);
+
+  return <DashboardLayout activeMenu="Manage Task"></DashboardLayout>;
 }
 
-export default ManageTasks
+export default ManageTasks;
